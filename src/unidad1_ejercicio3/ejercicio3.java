1759b38f2c4d236fed8cf9a888b2ffa58ca526d4
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-06 14:41:34 +0200

    first commit

---
+++ src/unidad1_ejercicio3/ejercicio3.java
@@ -0,0 +1,11 @@
+package unidad1_ejercicio3;
+
+public class ejercicio3 {
+
+	public static void main(String args[]) {
+		for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+			System.out.println("FIN DEL PROGRAMA");
+	}
+
+}
